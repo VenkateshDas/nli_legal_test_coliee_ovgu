e76faab880aff56a0c991388f4ec4fc7c7d332e7
Sachin Nandakumar <sachnandmenon@gmail.com>
2020-05-16 22:06:38 +0530

    Deleting duplicate. Changed folder structure.

--- sim_neg/download_google_news.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-
-url = 'https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz'       
-print("Downloading Google News Vectors...")
-urllib.request.urlretrieve(url, 'google-news.bin.gz')  
-print("Downloaded.")
